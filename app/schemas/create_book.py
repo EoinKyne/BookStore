d15b1ae0426d0daf4d17b0e19017207fbb7fcd6b
ekyne <eoin_kyne@yahoo.com>
2026-02-13 12:02:42 +0000

    fix for field example

--- app/schemas/create_book.py
+++ app/schemas/create_book.py
@@ -4,6 +4,15 @@ from pydantic import BaseModel, Field
 class CreateBook(BaseModel):
     title: str
     author: str
-    price: float = Field(..., gt=0, description="Price must positive", example="12.99")
+    price: float = Field(..., gt=0, description="Price must positive")
     stock: int = Field(..., ge=0, )
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "price": 12.99
+                }
+            ]
+        }
+    }
